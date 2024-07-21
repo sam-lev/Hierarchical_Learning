670cb83cb131737eb31d440edbb0c0fd352cfa90
multivax <multivax@multivaxs-mbp.lan>
2024-07-21 16:45:46 -0700

    added run exp

--- Hierarchical_GNN/run_experiments.sh
+++ Hierarchical_GNN/run_experiments.sh
@@ -28,7 +28,7 @@ python main.py --cuda_num=0 --dropout=0.2 --dim_hidden=512 --num_layers=5 \
 --lr=1e-4 --lr2=1e-4 --weight_decay=1e-8 \
 --data_subset=tolokers --persistence=0.8 --hier_model=HST 2>&1 | tee -- "$run_base/$run_path/$exp_name"
 }
-exp3
+#exp3
 exp_name="exp4_${name}_Wisconsin.log"
 exp4() {
 python main.py --cuda_num=0 --dropout=0.2 --dim_hidden=512 --num_layers=5 \
@@ -39,7 +39,7 @@ python main.py --cuda_num=0 --dropout=0.2 --dim_hidden=512 --num_layers=5 \
 --lr=1e-4 --lr2=1e-4 --weight_decay=1e-8 \
 --data_subset=Wisconsin --persistence=0.8 --hier_model=HST 2>&1 | tee -- "$run_base/$run_path/$exp_name"
 }
-#exp4
+exp4
 exp_name="exp5_${name}_tolokers.log"
 exp5() {
 python main.py --cuda_num=0 --dropout=0.2 --dim_hidden=512 --num_layers=5 \
@@ -50,7 +50,7 @@ python main.py --cuda_num=0 --dropout=0.2 --dim_hidden=512 --num_layers=5 \
 --lr=1e-4 --lr2=1e-4 --weight_decay=1e-12 \
 --data_subset=tolokers --persistence=0.8 --hier_model=HST 2>&1 | tee -- "$run_base/$run_path/$exp_name"
 }
-exp5
+#exp5
 exp_name="exp6_${name}_tolokers.log"
 exp6() {
 python main.py --cuda_num=0 --dropout=0.2 --dim_hidden=512 --num_layers=5 \
@@ -61,7 +61,8 @@ python main.py --cuda_num=0 --dropout=0.2 --dim_hidden=512 --num_layers=5 \
 --lr=1e-4 --lr2=1e-4 --weight_decay=0.0 \
 --data_subset=tolokers --persistence=0.8 --hier_model=HST 2>&1 | tee -- "$run_base/$run_path/$exp_name"
 }
-exp6
+#exp6
+
 #
 #/*
 #exp_2 () {
