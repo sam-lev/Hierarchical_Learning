6c49a79f5307700711c23afeb5dd6af4d7cb7876
Sam Leventhal <sam@leventhal.info>
2024-09-28 19:59:36 -0700

    doing planetoid param sweep

--- Hierarchical_GNN/multirun_experiments.sh
+++ Hierarchical_GNN/multirun_experiments.sh
@@ -152,8 +152,67 @@ done
 COMMENT
 
 ########################################################################################
+
+
 MODEL="HJT"
 
+
+
+#
+#           planetoid
+#
+for DATASET in Planetoid
+do
+	for DATASUBSET in CiteSeer #Cora PubMed
+	do
+		DATA_RUN_PATH="$run_base/$run_path/$MODEL/$DATASUBSET"
+		mkdir -p "$DATA_RUN_PATH"
+		for PERSISTENCE in "0.5,0.7,0.9" "0.3,0.9" "0.5,0.9" "0.6,0.8" "0.6,0.9" "0.8,0.9" "0.7" "0.8" "0.9" "0.1" #"0.1,0.2,0.3" #"0.9,0.8" "0.9,0.7" "0.8,0.7" "0.9,0.3" "0.8,0.3"
+		do
+			for LR in  3e-4 3e-3 0.03 1e-4 1e-3 0.01 #0.01 #0.03 0.0001
+			do
+				for LREDGE in 0.01 0.03 0.003 0.001 #0.01 #0.03 0.0001 0.01
+				do
+					for WD in 5e-5 5e-8 5e-4 0 1e-12 #1e-12
+					do
+			    			exp_name="exp_${name}_${MODEL}_${DATASET}_${DATASUBSET}_LR${LR}_LRE_${LREDGE}_WD${WD}_PERS${PERSISTENCE}.log"
+			    			RUN_FILE="$DATA_RUN_PATH/$exp_name"
+
+			    			echo "MODEL=${MODEL}"
+			    			echo "MODEL=${MODEL}" >> "$RUN_FILE"
+			    			echo "DATASET=${DATASET}"
+			    			echo "DATASET=${DATASET}" >> "$RUN_FILE"
+			    			echo "DATASUBSET=${DATASUBSET}"
+			    			echo "DATASUBSET=${DATASUBSET}" >> "$RUN_FILE"
+			    			echo "PERSISTENCE=${PERSISTENCE}"
+			    			echo "PERSISTENCE=${PERSISTENCE}" >> "$RUN_FILE"
+			    			echo "LREDGE=${LREDGE}"
+			    			echo "LREDGE=${LREDGE}" >> "$RUN_FILE"
+			    			echo "LR=${LR}"
+			    			echo "LR=${LR}" >> "$RUN_FILE"
+			    			echo "WD=${WD}"
+			    			echo "WD=${WD}" >> "$RUN_FILE"
+
+			    			#> "$RUN_FILE"
+
+						experiment "$DATASET" "$DATASUBSET" "${PERSISTENCE}" "${LR}" "${LREDGE}" "${WD}" "$RUN_FILE" "$MODEL"
+					done
+				done
+			done
+		done
+		DATA_RESULTS_PATH="$run_base/$run_path/$MODEL/RESULTS"
+		mkdir -p "$DATA_RESULTS_PATH"
+		RESULTS_FILE="$DATA_RESULTS_PATH/$DATASUBSET_${name}_RESULTS_SUMMARY.log"
+		python print_log_file_results.py "$DATA_RUN_PATH" 2>&1 | tee -- "$RESULTS_FILE"
+		OPT_RESULTS_FILE="$DATA_RESULTS_PATH/$DATASUBSET_${name}_OPT_RESULTS.log"
+		python best_test_accuracy.py "$RESULTS_FILE" 2>&1 | tee -- "$OPT_RESULTS_FILE"
+
+		OPT_ROC_RESULTS_FILE="$DATA_RESULTS_PATH/$DATASUBSET_${name}_OPT_ROC_RESULTS.log"
+		python best_roc_auc.py "$RESULTS_FILE" 2>&1 | tee -- "$OPT_ROC_RESULTS_FILE"
+	done
+done
+
+: << COMMENT
 for DATASET in HeterophilousGraphDataset #Planetoid #WikipediaNetwork #WebKB
 do
 	for DATASUBSET in  roman_empire minesweeper questions amazon_ratings tolokers questions #squirrel  # chameleon # for DATASUBSET in texas # wisconsin cornell    
