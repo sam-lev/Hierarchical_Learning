5d1b037d6676f1e1e4574b2ddf7f6a00c1bd0b62
Sam Leventhal <sam@leventhal.info>
2024-09-30 15:54:47 -0700

    added avg learned filt value exp

--- Hierarchical_GNN/explicit_experimental_runs.sh
+++ Hierarchical_GNN/explicit_experimental_runs.sh
@@ -5,7 +5,7 @@
 run_base=$(pwd)
 run_path="runs"
 
-name="avgFilt_Exp_"
+name="_FiltVal_exp_"
 
 # best pubmed hst exp_hjt_21_HST_Planetoid_PubMed_LR0.01_LRE_0.01_WD5e-5_PERS0.7,0.9.log
 # best for hjt exp_hjt_9_HJT_Planetoid_PubMed_LR3e-4_LRE_0.03_WD5e-8_PERS0.5,0.7,0.9.log
@@ -22,15 +22,15 @@ hjt_ablation_experiment() {
 	exp_name="exp_${EXPERIMENT}_${MODEL}_${DATASET}_${DATASUBSET}_LR${LR}_LRE_${LREDGE}_WD${WD}_PERS${PERSISTENCE}.log"
   RUN_FILE="$DATA_RUN_PATH/$exp_name"
 
-	PERSISTENCE="0.4,0.6,0.8"
+	PERSISTENCE="0.5,0.7,0.9"
 	python main.py --cuda_num=0 \
-	--dropout_edge=0.4 --dim_hidden_edge=256 \
-	--dropout=0.4 --dim_hidden=256 --dim_gin=256 --dim_multiscale_filter_conv=256 --num_layers=3 \
+	--dropout_edge=0.3 --dim_hidden_edge=64 \
+	--dropout=0.6 --dim_hidden=256 --dim_gin=256 --dim_multiscale_filter_conv=256 --num_layers=3 \
 	--batch_size=512 --use_batch_norm=True --SLE_threshold=0.9 --N_exp=1 \
-	--dataset=Planetoid --epochs=121 --homophily=0.9 \
+	--dataset=Planetoid --epochs=321 --homophily=0.9 \
 	--multi_label=True --type_model=HierGNN \
 	--eval_steps=8 --train_by_steps=False \
-	--lr=0.003 --lr2=0.1 --weight_decay=1e-4 \
+	--lr=3e-4 --lr2=0.03 --weight_decay=5e-8 \
 	--data_subset=PubMed --persistence="${PERSISTENCE}" --hier_model=HJT 2>&1 | tee -- "$RUN_FILE" \
 	--experiment=None
 
@@ -38,12 +38,12 @@ hjt_ablation_experiment() {
   mkdir -p "$DATA_RUN_PATH"
 	exp_name="exp_${EXPERIMENT}_${MODEL}_${DATASET}_${DATASUBSET}_LR${LR}_LRE_${LREDGE}_WD${WD}_PERS${PERSISTENCE}.log"
   RUN_FILE="$DATA_RUN_PATH/$exp_name"
-	PERSISTENCE="0.4,0.6,0.8"
+	PERSISTENCE="0.5,0.7,0.9"
 	python main.py --cuda_num=0 \
-	--dropout_edge=0.4 --dim_hidden_edge=256 \
-	--dropout=0.4 --dim_hidden=256 --dim_gin=256 --dim_multiscale_filter_conv=256 --num_layers=3 \
+	--dropout_edge=0.3 --dim_hidden_edge=64 \
+	--dropout=0.6 --dim_hidden=256 --dim_gin=256 --dim_multiscale_filter_conv=256 --num_layers=3 \
 	--batch_size=512 --use_batch_norm=True --SLE_threshold=0.9 --N_exp=1 \
-	--dataset=Planetoid --epochs=121 --homophily=0.9 \
+	--dataset=Planetoid --epochs=321 --homophily=0.9 \
 	--multi_label=True --type_model=HierGNN \
 	--eval_steps=8 --train_by_steps=False \
 	--lr=0.003 --lr2=0.1 --weight_decay=1e-4 \
