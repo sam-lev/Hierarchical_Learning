aea35f7b638eb214151c04bb4febb026b6a80b8f
Sam Leventhal <sam@leventhal.info>
2024-09-30 17:13:03 -0700

    fsdfs

--- Hierarchical_GNN/explicit_experimental_runs.sh
+++ Hierarchical_GNN/explicit_experimental_runs.sh
@@ -24,13 +24,13 @@ hjt_ablation_experiment() {
 
 	PERSISTENCE="0.5,0.7,0.9"
 	python main.py --cuda_num=0 \
-	--dropout_edge=0.4 --dim_hidden_edge=32 \
+	--dropout_edge=0.5 --dim_hidden_edge=32 \
 	--dropout=0.6 --dim_hidden=256 --dim_gin=256 --dim_multiscale_filter_conv=256 --num_layers=3 \
 	--batch_size=64 --use_batch_norm=True --SLE_threshold=0.9 --N_exp=1 \
 	--dataset=Planetoid --epochs=321 --homophily=0.9 \
 	--multi_label=True --type_model=HierGNN \
 	--eval_steps=8 --train_by_steps=False \
-	--lr=3e-4 --lr2=0.0003 --weight_decay=5e-5 \
+	--lr=3e-4 --lr2=0.003 --weight_decay=5e-5 \
 	--data_subset=PubMed --persistence="${PERSISTENCE}" --hier_model=HJT 2>&1 | tee -- "$RUN_FILE" \
 	--experiment=None
 
